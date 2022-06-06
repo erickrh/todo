ad3042d11befb059553e076cc05a88b69c7080be
Erick <123erickrh@gmail.com>
2022-06-06 10:13:33 -0500

    Creación de componentes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { TodoCounter } from './TodoCounter';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+// import './App.css';
 
-function App(props) {
+const todos = [
+  { text: 'Cortar cebolla', completed: false },
+  { text: 'Cocinar', completed: false },
+  { text: 'Renderizar', completed: false },
+];
+
+function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. It work!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {props.saludo}
-          <br />
-          <br />
-          {props.children}
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+
+      <TodoCounter />
+
+      <TodoSearch />
+
+      <TodoList>
+        {todos.map(todo => (
+          <TodoItem key = {todo.text} text = {todo.text} />
+        ))}
+      </TodoList>
+
+      <CreateTodoButton />
+
+    </React.Fragment>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,17 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-function myApp() {
-  return (
-    <h1 id='title'>
-      Hello world.
-    </h1>
-  );
-}
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App saludo = "Hola mundo">
-      Hello these are the children
-    </App>
+    <App />
 );
