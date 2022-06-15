cfc63ed41c6e2303e22c6f0053ef6d6c86795b96
Erick <123erickrh@gmail.com>
2022-06-14 21:59:04 -0500

    Se añade el estado en TodoSearch

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -4,21 +4,28 @@ import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons';
 
 function TodoSearch() {
+    const [searchValue, setSearchValue] = React.useState('');
+
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
+        setSearchValue(event.target.value); // Se actualizará el estado.
     };
 
-    return (
-        <div className='TodoSearchContainer'>
-            {/* Search Icon */}
-            <FontAwesomeIcon icon = {faMagnifyingGlass} className='iconSearch' />
+    return [
+        <React.Fragment>
+            <div className='TodoSearchContainer'>
+                {/* Search Icon */}
+                <FontAwesomeIcon icon = {faMagnifyingGlass} className='iconSearch' />
 
-            <input
-            autoFocus className='TodoSearch' type="text" placeholder="Buscar"
-            onChange={onSearchValueChange}
-            />
-        </div>
-    )
+                <input
+                autoFocus className='TodoSearch' type="text" placeholder="Buscar"
+                value={searchValue} // React exige conectar el valor de nuestro input con el estado.
+                onChange={onSearchValueChange} // En cada cambio actualiza el estado.
+                />
+            </div>
+            <p>{searchValue}</p>
+        </React.Fragment>
+    ]
 }
 
 export { TodoSearch };
