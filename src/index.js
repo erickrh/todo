2626a58f45fbbe16a4a4dbc43edf89e366de3962
Erick <123erickrh@gmail.com>
2022-10-10 17:50:52 -0500

    HOC Example

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+
+function App(props) {
+  return (
+    <h1>¡{props.saludo}, {props.name}!</h1>
+  );
+}
+
+// HOC
+function withSaludo(WrappedComponent) {
+  return function WrappedComponentWithSaludo(saludo) {
+    return function componenteDeVerdad(props) {
+      return (
+        <React.Fragment>
+          <WrappedComponent {...props} saludo={saludo} />
+          <p>HOC was here.</p>
+        </React.Fragment>
+      );
+    };
+  };
+}
+
+const AppWithSaludo = withSaludo(App)('Hello world');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  // <App />
+  <AppWithSaludo name='Erick' />
 );
